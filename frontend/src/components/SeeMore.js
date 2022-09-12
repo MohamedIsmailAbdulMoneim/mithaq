2c12bbb8e2ca3c4c1500ec10b0f9651ddf196fd1
Mohamed Ismail Abdul-Monein <mohamedmasrawe@gmail.com>
2022-09-12 20:28:14 +0200

    fixing easy error

--- frontend/src/components/SeeMore.js
+++ frontend/src/components/SeeMore.js
@@ -18,15 +18,15 @@ const SeeMore = ({ inputs, data }) => {
   const { id } = useParams()
   const [memberDetails] = data.filter(x => x.id === parseInt(id))
   const arrOfNums = memberDetails.phoneNumbers?.split(',,') || []
-    arrOfNums[arrOfNums?.length -1] = arrOfNums[arrOfNums.length-1]?.substr(0, arrOfNums[arrOfNums.length-1].length - 1)
-  .map(x => JSON.parse(x))
+  arrOfNums[arrOfNums?.length - 1] = arrOfNums[arrOfNums.length - 1]?.substr(0, arrOfNums[arrOfNums.length - 1].length - 1)
   
+
   return (
-    <Box sx={{ width: 950,  margin: '0 auto', backgroundColor: '#efe4e6', boxShadow: 'rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;' }}>
+    <Box sx={{ width: 950, margin: '0 auto', backgroundColor: '#efe4e6', boxShadow: 'rgba(255, 255, 255, 0.2) 0px 0px 0px 1px inset, rgba(0, 0, 0, 0.9) 0px 0px 0px 1px;' }}>
       <Grid container sx={{ padding: '0 90px' }} spacing={2}>
         {inputs.map(x => (
           x.field === 'phoneNumber' ?
-          arrOfNums.map(x => JSON.parse(x))?.map(a => (
+            arrOfNums.map(x => JSON.parse(x))?.map(a => (
               <Grid item lg="12">
                 <TextField
                   aria-readonly
@@ -59,7 +59,7 @@ const SeeMore = ({ inputs, data }) => {
                 />
               </Grid>
         ))}
-        <Grid item lg="12" style={{marginBottom: 10}}>
+        <Grid item lg="12" style={{ marginBottom: 10 }}>
           <ColorButton fullWidth variant="contained"><Link style={{ color: 'white' }} to={`/edit/${id}`}>تعديل البيانات</Link></ColorButton>
         </Grid>
       </Grid>
