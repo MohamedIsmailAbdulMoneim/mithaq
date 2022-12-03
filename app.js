b77ab8dc4648c6cfcafc0a32546aa7e3024e7466
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2022-12-03 14:55:40 +0200

    s

--- app.js
+++ app.js
@@ -86,10 +86,11 @@ const editRecord = (req, res) => {
 
     const { id } = filteredNullData
     const reAranged = phoneNums.filter(x => x.phoneNumber.length > 2)
-        if(filteredNullData.contract_date = ""){
-            console.log(true);
-            filteredNullData.contract_date = '0000-00-00'
-        }
+    console.log(filteredNullData.contract_date)
+        // if(filteredNullData.contract_date = ""){
+        //     console.log(true);
+        //     filteredNullData.contract_date = '0000-00-00'
+        // }
     delete filteredNullData.id
     delete filteredNullData.s
     delete filteredNullData.phoneNumber
