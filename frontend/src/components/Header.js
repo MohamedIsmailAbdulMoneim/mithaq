f5ae939d510cac6ff012ad7dbf057359706a3351
MohamedIsmailAbdulMoneim <mohamedmasrawe@gmail.com>
2023-03-03 14:11:43 +0200

    m

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,19 @@
 import * as React from 'react';
-import { styled as style, alpha } from '@mui/material/styles';
+// import { styled as style, alpha } from '@mui/material/styles';
 import styled from 'styled-components'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MoreIcon from '@mui/icons-material/MoreVert';
+// import IconButton from '@mui/material/IconButton';
+// import InputBase from '@mui/material/InputBase';
+// import Badge from '@mui/material/Badge';
+// import MenuItem from '@mui/material/MenuItem';
+// import Menu from '@mui/material/Menu';
+// import AccountCircle from '@mui/icons-material/AccountCircle';
+// import MailIcon from '@mui/icons-material/Mail';
+// import NotificationsIcon from '@mui/icons-material/Notifications';
 import logo from '../logo.jpg'
-import { create } from 'jss';
-import rtl from 'jss-rtl';
-import { StylesProvider, jssPreset } from '@mui/styles';
+// import { create } from 'jss';
+// import rtl from 'jss-rtl';
+// import {  jssPreset } from '@mui/styles';
 import { Link } from 'react-router-dom';
-import { margin } from '@mui/system';
 
 const Button = styled.button`
 background: inherit;
@@ -37,237 +29,250 @@ cursor: pointer
 `
 
 // Configure JSS
-const jss = create({
-  plugins: [...jssPreset().plugins, rtl()],
-});
+// const jss = create({
+//   plugins: [...jssPreset().plugins, rtl()],
+// });
 
 
-function RTL(props) {
-  return <StylesProvider jss={jss}>{props.children}</StylesProvider>;
-}
+// function RTL(props) {
+//   return <StylesProvider jss={jss}>{props.children}</StylesProvider>;
+// }
 
 
 
 
-const Search = style('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(3),
-    width: 'auto',
-  },
-}));
+// const Search = style('div')(({ theme }) => ({
+//   position: 'relative',
+//   borderRadius: theme.shape.borderRadius,
+//   backgroundColor: alpha(theme.palette.common.white, 0.15),
+//   '&:hover': {
+//     backgroundColor: alpha(theme.palette.common.white, 0.25),
+//   },
+//   marginRight: theme.spacing(2),
+//   marginLeft: 0,
+//   width: '100%',
+//   [theme.breakpoints.up('sm')]: {
+//     marginLeft: theme.spacing(3),
+//     width: 'auto',
+//   },
+// }));
 
-const SearchIconWrapper = style('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
+// const SearchIconWrapper = style('div')(({ theme }) => ({
+//   padding: theme.spacing(0, 2),
+//   height: '100%',
+//   position: 'absolute',
+//   pointerEvents: 'none',
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'center',
+// }));
 
-const StyledInputBase = style(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-}));
+// const StyledInputBase = style(InputBase)(({ theme }) => ({
+//   color: 'inherit',
+//   '& .MuiInputBase-input': {
+//     padding: theme.spacing(1, 1, 1, 0),
+//     // vertical padding + font size from searchIcon
+//     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+//     transition: theme.transitions.create('width'),
+//     width: '100%',
+//     [theme.breakpoints.up('md')]: {
+//       width: '20ch',
+//     },
+//   },
+// }));
 
-export default function PrimarySearchAppBar({user, logoutHandler}) {
+export default function PrimarySearchAppBar({ user, logoutHandler }) {
   console.log(user);
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  // const [setAnchorEl] = React.useState(null);
+  // const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  // const isMenuOpen = Boolean(anchorEl);
+  // const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+  // const handleProfileMenuOpen = (event) => {
+  //   setAnchorEl(event.currentTarget);
+  // };
 
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
+  // const handleMobileMenuClose = () => {
+  //   setMobileMoreAnchorEl(null);
+  // };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
+  // const handleMenuClose = () => {
+  //   setAnchorEl(null);
+  //   handleMobileMenuClose();
+  // };
 
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
+  // const handleMobileMenuOpen = (event) => {
+  //   setMobileMoreAnchorEl(event.currentTarget);
+  // };
 
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
-    </Menu>
-  );
+  // const menuId = 'primary-search-account-menu';
+  // const renderMenu = (
+  //   <Menu
+  //     anchorEl={anchorEl}
+  //     anchorOrigin={{
+  //       vertical: 'top',
+  //       horizontal: 'right',
+  //     }}
+  //     id={menuId}
+  //     keepMounted
+  //     transformOrigin={{
+  //       vertical: 'top',
+  //       horizontal: 'right',
+  //     }}
+  //     open={isMenuOpen}
+  //     onClose={handleMenuClose}
+  //   >
+  //     <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+  //     <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+  //   </Menu>
+  // );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-          <Badge badgeContent={4} color="error">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>Messages</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>Notifications</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
-          <AccountCircle />
-        </IconButton>
-        <p>Profile</p>
-      </MenuItem>
-    </Menu>
-  );
+  // const mobileMenuId = 'primary-search-account-menu-mobile';
+  // const renderMobileMenu = (
+  //   <Menu
+  //     anchorEl={mobileMoreAnchorEl}
+  //     anchorOrigin={{
+  //       vertical: 'top',
+  //       horizontal: 'right',
+  //     }}
+  //     id={mobileMenuId}
+  //     keepMounted
+  //     transformOrigin={{
+  //       vertical: 'top',
+  //       horizontal: 'right',
+  //     }}
+  //     open={isMobileMenuOpen}
+  //     onClose={handleMobileMenuClose}
+  //   >
+  //     <MenuItem>
+  //       <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+  //         <Badge badgeContent={4} color="error">
+  //           <MailIcon />
+  //         </Badge>
+  //       </IconButton>
+  //       <p>Messages</p>
+  //     </MenuItem>
+  //     <MenuItem>
+  //       <IconButton
+  //         size="large"
+  //         aria-label="show 17 new notifications"
+  //         color="inherit"
+  //       >
+  //         <Badge badgeContent={17} color="error">
+  //           <NotificationsIcon />
+  //         </Badge>
+  //       </IconButton>
+  //       <p>Notifications</p>
+  //     </MenuItem>
+  //     <MenuItem onClick={handleProfileMenuOpen}>
+  //       <IconButton
+  //         size="large"
+  //         aria-label="account of current user"
+  //         aria-controls="primary-search-account-menu"
+  //         aria-haspopup="true"
+  //         color="inherit"
+  //       >
+  //         <AccountCircle />
+  //       </IconButton>
+  //       <p>Profile</p>
+  //     </MenuItem>
+  //   </Menu>
+  // );
 
   return (
-    <RTL>
-      <Box dir="rtl" sx={{ flexGrow: 1, marginBottom: 12 }}>
-        <AppBar position="fixed" sx={{ backgroundColor: '#310109' }}>
-          <Toolbar>
+    <div style={{ background: "#310109", color: 'white', width: "100%", maxHeight: 64, marginBottom: 20, padding: 10, display: "flex", justifyContent: "space-between" }}>
 
-            {/* <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton> */}
+      {user === "methaqfamily" && <Link to={'/register'}><Button>مستخدم جديد</Button></Link>}
+      {user && <Button onClick={logoutHandler}>تسجيل خروج</Button>}
 
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              sx={{ display: { xs: 'none', sm: 'block' } }}
+      <Link to='/'><img src={logo} alt="logo" style={{ maxWidth: 50, maxHeight: 50 }} /></Link>
+      <div style={{ lineHeight: 3, textTransform: "uppercase", color: "#f0e4b6" }}>
+        <span style={{ marginRight: 5 }}>
+          {user}
+        </span>
+        مرحبا يا
+      </div>
+    </div>
+    // <RTL>
+    //   <Box dir="rtl" sx={{ flexGrow: 1, marginBottom: 12 }}>
+    //     <AppBar position="fixed" sx={{ backgroundColor: '#310109' }}>
+    //       <Toolbar>
+
+    //         {/* <IconButton
+    //           size="large"
+    //           edge="start"
+    //           color="inherit"
+    //           aria-label="open drawer"
+    //           sx={{ mr: 2 }}
+    //         >
+    //           <MenuIcon />
+    //         </IconButton> */}
+
+    //         <Typography
+    //           variant="h6"
+    //           noWrap
+    //           component="div"
+    //           sx={{ display: { xs: 'none', sm: 'block' } }}
 
 
-            >
-              <Link style={{ margin: '0 auto' }} to='/'><img src={logo} alt="logo" style={{ width: 50, height: 50, position: 'absolute', top: '5%', right: '50%' }} /></Link>
-            </Typography>
-            {/* <Search >
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase sx={{ marginRight: 5 }}
-                placeholder="البـحــث…"
-                inputProps={{ 'aria-label': 'search' }}
-              />
-            </Search> */}
-            <Box sx={{ flexGrow: 1 }} />
-            {/* <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+    //         >
+    //           <Link style={{ margin: '0 auto' }} to='/'><img src={logo} alt="logo" style={{ width: 50, height: 50, position: 'absolute', top: '5%', right: '50%' }} /></Link>
+    //         </Typography>
+    //         {/* <Search >
+    //           <SearchIconWrapper>
+    //             <SearchIcon />
+    //           </SearchIconWrapper>
+    //           <StyledInputBase sx={{ marginRight: 5 }}
+    //             placeholder="البـحــث…"
+    //             inputProps={{ 'aria-label': 'search' }}
+    //           />
+    //         </Search> */}
+    //         <Box sx={{ flexGrow: 1 }} />
+    //         {/* <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
 
-              <IconButton
-                size="large"
-                aria-label="show 17 new notifications"
-                color="inherit"
-              >
-                <Badge badgeContent={17} color="error">
-                  <NotificationsIcon />
-                
-                </Badge>
-              </IconButton>
-              <IconButton
-                size="large"
-                edge="end"
-                aria-label="account of current user"
-                aria-controls={menuId}
-                aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-            </Box> */}
-            {/* <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
-              <IconButton
-                size="large"
-                aria-label="show more"
-                aria-controls={mobileMenuId}
-                aria-haspopup="true"
-                onClick={handleMobileMenuOpen}
-                color="inherit"
-              >
-                <MoreIcon />
-              </IconButton>
-            </Box> */}
-            {user === "methaqfamily" && <Link to={'/register'}><Button>مستخدم جديد</Button></Link>}
-            {user && <Button onClick={logoutHandler}>تسجيل خروج</Button>}
-            
-          </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
-      </Box>
-    </RTL>
+    //           <IconButton
+    //             size="large"
+    //             aria-label="show 17 new notifications"
+    //             color="inherit"
+    //           >
+    //             <Badge badgeContent={17} color="error">
+    //               <NotificationsIcon />
+
+    //             </Badge>
+    //           </IconButton>
+    //           <IconButton
+    //             size="large"
+    //             edge="end"
+    //             aria-label="account of current user"
+    //             aria-controls={menuId}
+    //             aria-haspopup="true"
+    //             onClick={handleProfileMenuOpen}
+    //             color="inherit"
+    //           >
+    //             <AccountCircle />
+    //           </IconButton>
+    //         </Box> */}
+    //         {/* <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+    //           <IconButton
+    //             size="large"
+    //             aria-label="show more"
+    //             aria-controls={mobileMenuId}
+    //             aria-haspopup="true"
+    //             onClick={handleMobileMenuOpen}
+    //             color="inherit"
+    //           >
+    //             <MoreIcon />
+    //           </IconButton>
+    //         </Box> */}
+    //         {user === "methaqfamily" && <Link to={'/register'}><Button>مستخدم جديد</Button></Link>}
+    //         {user && <Button onClick={logoutHandler}>تسجيل خروج</Button>}
+
+    //       </Toolbar>
+    //     </AppBar>
+    //     {renderMobileMenu}
+    //     {renderMenu}
+    //   </Box>
+    // </RTL>
   );
 }
